d93a94cff83032f8304c357282b4795ef1c771fa
Diego R Góes <diego@vsktechnology.com.br>
2022-09-26 21:39:35 -0300

    Modo escuro adicionado

--- gerenciador_de_senhas.js
+++ gerenciador_de_senhas.js
@@ -9,16 +9,49 @@
 //     console.log(input_atual)
 // }
 
-document.querySelectorAll('.input_btn').forEach((e)=>{
-    e.addEventListener('click',()=>{
-        e.classList.toggle('teste')
-        setTimeout(()=>{e.classList.toggle('teste')},200)
+function mudar_tema() {
+    let tag_html = document.querySelector('html')
+    tag_html.classList.toggle('dark-mode')
+    let status_tema = document.querySelector('#status_tema')
+    let code_icon = ''
+    let primary_color = ''
+    let secondary_color = ''
+
+    if (tag_html.classList.contains('dark-mode')) {
+        code_icon = 'tgnqhsfe'
+        primary_color = '#ffffff'
+        secondary_color = '#a39cf4'
+        status_tema.textContent = 'Escuro'
+    } else {
+        status_tema.textContent = 'Claro'
+        code_icon = 'lytdihcu'
+        primary_color = '#121331'
+        secondary_color = '#545454'
+    }
+    document.querySelector('#icon_tema').attributes[0].nodeValue = `https://cdn.lordicon.com/${code_icon}.json`
+    document.querySelectorAll('lord-icon').forEach((lord_icon_atual) => {
+        lord_icon_atual.attributes.colors.textContent = `primary:${primary_color},secondary:${secondary_color}`
+    })
+}
+// link attribute href value
+let other_options = document.querySelectorAll('.other_options')
+other_options.forEach((divElement) => {
+
+    let btn_more_options = divElement.children[0]
+    btn_more_options.addEventListener('click', () => {
+
+        // Verificar se existem outros botões visíveis além do escolhido
+        other_options.forEach((e) => {
+            if (e.classList.contains('show_other_options') && e !== divElement) {
+                e.classList.remove('show_other_options')
+            }
+        })
+
+        // Add / remove classe que da visibilidade
+        divElement.classList.toggle('show_other_options')
     })
 })
-let btn_imporatar = document.querySelector('#btn_importar')
-btn_imporatar.addEventListener('click',(event)=>{
-    btn_imporatar.classList.toggle('clique_vazado')
-})
+
 // Ideias de funcionalidades
 // Gerador de chave aleatória
 // Filtro de senhas
